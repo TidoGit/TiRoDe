617b84d1dfff6315db1cf2cd40a65c26bc24153a
Roel <roelfoolen@live.nl>
2022-12-12 10:30:49 +0100

    Fixed typo

--- roel/python/training1-2.py
+++ roel/python/training1-2.py
@@ -1,5 +1,5 @@
 name = input("Hello! What's your name? ")
 name = name.title()
-age = input(" Hello %s! What's your age? " % name)
+age = input("Hello %s! What's your age? " % name)
 
 print('So, your name is %s and you are %s years old!' % (name, int(age)))
