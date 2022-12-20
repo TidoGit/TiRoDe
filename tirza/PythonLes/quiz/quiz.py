afcafe38a49a9ecba886ae089ae62d812ca5dbd2
TidoGit <chucknorris@xs4all.nl>
2022-12-20 12:07:14 +0100

    minor change

---
+++ tirza/PythonLes/quiz/quiz.py
@@ -0,0 +1,49 @@
+'''
+Reading input from the user
+
+Organizing data in structures like lists and dictionaries
+
+Using if statements to check different conditions
+
+Repeating actions with for and while loops
+
+Encapsulating code with functions'''
+
+#VARIABLES
+user_name = input("who's playing?")
+answer = input("Question here")
+correct_answer = 0
+
+#FUNCTION TO ADD QUESTIONS
+questions = [{
+    "Question" : "question",
+    "A"  : "string",
+    "B"  : "iets"
+    "C"  : "iets anders"
+    "Answer"   : "A",
+},{
+    "Question" : "question",
+    "Options"  : "options",
+    "Answer"   : "B",
+}]
+
+#OPEN AND WRITE TO FILE
+with open:
+    iets.json
+
+
+#CHECK ANSWER FUNCTION, check correct input format.
+
+if answer == "input":
+    correct_answer += 1
+    print("Correct!")
+else:
+    print(f"The answer is "input", not {answer!r}")
+
+#KEEP SCORE FUNCTION
+user_name + correct_answer
+
+
+
+
+
