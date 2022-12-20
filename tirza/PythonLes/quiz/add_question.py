350700594653b31e11da0e97a7936ce1c3915e36
Roel <roelfoolen@live.nl>
2022-12-20 13:13:28 +0100

    fixed .json file and import/export in add_question.py

--- tirza/PythonLes/quiz/add_question.py
+++ tirza/PythonLes/quiz/add_question.py
@@ -1,9 +1,16 @@
 #function to add questions to our quiz
 import json
 
-questions = []
 
-def add_question():    
+def import_questions():
+    '''Import questions from 'questions.json'.'''
+    with open('questions.json', 'r', encoding='UTF-8') as file:
+        questions = json.load(file)
+    return questions
+
+
+def add_question():
+    questions = import_questions()
     question = input("Enter Question: \n")
     a = input("Enter Answer A: \n")
     b = input("Enter Answer B: \n")
@@ -12,8 +19,10 @@ def add_question():
     answer = input("Correct answer (A,B,C or D)")
     entry = {"question" : question,"a" : a,"b" : b,"c" : c,"d" : d,"answer" : answer,}
     questions.append(entry)
-    with open('questions.json', 'a', encoding='UTF-8') as file:
-        json.dump(questions, file)
+    with open('questions.json', 'w', encoding='UTF-8') as file:
+        json.dump(questions, file, indent = 4)
+
+
 add_question()
 
 
