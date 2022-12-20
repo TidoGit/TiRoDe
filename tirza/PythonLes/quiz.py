69bb27235c50bcc83b33e92d396170d6472b9a62
TidoGit <chucknorris@xs4all.nl>
2022-12-20 11:38:54 +0100

    latest

--- tirza/PythonLes/quiz.py
+++ tirza/PythonLes/quiz.py
@@ -9,22 +9,40 @@ Repeating actions with for and while loops
 
 Encapsulating code with functions'''
 
-#variables
+#VARIABLES
 user_name = input("who's playing?")
 answer = input("Question here")
+correct_answer = 0
+
+#FUNCTION TO ADD QUESTIONS
 questions = [{
-    "Question" : "question"
-    "Options"  : "options"
-    "Answer"   : "answer"
+    "Question" : "question",
+    "A"  : "string",
+    "B"  : "iets"
+    "C"  : "iets anders"
+    "Answer"   : "A",
+},{
+    "Question" : "question",
+    "Options"  : "options",
+    "Answer"   : "B",
 }]
 
-#check answer function
+#OPEN AND WRITE TO FILE
+with open:
+    iets.json
+
+
+#CHECK ANSWER FUNCTION, check correct input format.
+
 if answer == "input":
+    correct_answer += 1
     print("Correct!")
 else:
-    print(f"The answer is 'input', not {answer!r}")
+    print(f"The answer is "input", not {answer!r}")
 
-#keep score function
+#KEEP SCORE FUNCTION
+user_name + correct_answer
+
 
 
 
