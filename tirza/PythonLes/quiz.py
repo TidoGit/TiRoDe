b9023ac16c7466c26f04750f647f496a6148d26f
TidoGit <chucknorris@xs4all.nl>
2022-12-20 10:35:23 +0100

    new quiz

---
+++ tirza/PythonLes/quiz.py
@@ -0,0 +1,27 @@
+'''
+Reading input from the user
+
+Organizing data in structures like lists and dictionaries
+
+Using if statements to check different conditions
+
+Repeating actions with for and while loops
+
+Encapsulating code with functions'''
+
+#variables
+user_name = input("who's playing?")
+answer = input("Question here")
+questions = [("question", "answer")]
+
+#check answer function
+if answer == "input":
+    print("Correct!")
+else:
+    print(f"The answer is 'input', not {answer!r}")
+
+#keep score function
+
+
+
+
